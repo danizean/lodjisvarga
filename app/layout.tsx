21be6bb2432ff876352e0b41be94d4cd30b22eb3
halvest <hasyimadani@students.amikom.ac.id>
2026-04-05 22:37:32 +0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import "../styles/globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata = {
   title: "Lodjisvarga Villa",
@@ -11,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
+    <html lang="id" className={cn("font-sans", geist.variable)}>
       <body>{children}</body>
     </html>
   );
