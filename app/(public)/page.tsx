d993dcee5f4a827a81a12b2ef0e28add60155f8b
halvest <hasyimadani@students.amikom.ac.id>
2026-04-07 03:59:25 +0700

    feat: initial commit

--- app/(public)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Lodjisvarga Villa</h1>
-    </div>
-  );
-}
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
-// postcss.config.mjs
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {}, // Gunakan ini sebagai pengganti 'tailwindcss'
+    "@tailwindcss/postcss": {},
   },
 };
 
